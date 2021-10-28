9f0434334b14600ae4f569d74ab4e779d1e219c4
unknown <benoit.pairet@mil.be>
2021-10-28 14:56:54 +0200

    __init__ is updated

--- __init__.py
+++ __init__.py
@@ -14,17 +14,15 @@
     GNU General Public License for more details.
 '''
 
-
-import numpy as np
-import vip_hci as vip
-
-
 from mayo_hci.operators import *
 from mayo_hci.algo_utils import *
 
 from mayo_hci.mayonnaise import *
 
+from mayo_hci.bootstrap import *
+
 from mayo_hci.automatic_load_data import *
 from mayo_hci.create_synthetic_data_with_disk_planet import *
 from mayo_hci.GreeDS import *
 
+from mayo_hci.spicyMayo import *
